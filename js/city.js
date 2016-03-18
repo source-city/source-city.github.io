38060c41dd3bc5251ed35b084c9dbba51d3a054c
mpi <mpi.michal.piotrkowski@gmail.com>
2016-03-19 00:46:12 +0100

    Deployment Sat Mar 19 2016 00:46:07

--- js/city.js
+++ js/city.js
@@ -7,10 +7,22 @@ define(['vendor/three'], function (THREE) {
 
     var scene = new THREE.Scene();
 
+    var geometry, material;
+
+    geometry = new THREE.BoxGeometry(2000, 10, 2000);
+    material = new THREE.MeshLambertMaterial({
+      color: 0xffffff
+    });
+
+    var floor = new THREE.Mesh(geometry, material);
+    floor.receiveShadow = true;
+    floor.translateY(-10);
+    scene.add(floor);
+
     scene.addBuilding = function (building) {
-      
+
       var geometry, material, mesh;
-      
+
       geometry = new THREE.BoxGeometry(building.foundations, building.height, building.foundations);
       material = new THREE.MeshLambertMaterial({
         color: 0xff0000
@@ -18,6 +30,9 @@ define(['vendor/three'], function (THREE) {
 
       mesh = new THREE.Mesh(geometry, material);
       mesh.receiveShadow = true;
+      mesh.translateY(building.height / 2);
+      mesh.translateX(building.x);
+      mesh.translateZ(building.y);
       scene.add(mesh);
     };
 
--- js/main.js
+++ js/main.js
@@ -16,16 +16,59 @@ requirejs.config({
 
 require(['city-architect'], function (cityArchitect) {
 
-  var data = [
-    {
-      label: 'com.bla.BlaBla',
-      metrics: {
-        loc: 100,
-        dependencies: 7
-      }
-    }
-  ];
+//  var data = [
+//    {
+//      label: 'com.bla.BlaBla',
+//      metrics: {
+//        loc: 100,
+//        dependencies: 7
+//      }
+//    }
+//  ];
 
+  var data = [
+    { foundations: 100, height: 200},
+    { foundations: 150, height: 300},
+    { foundations: 250, height: 150},
+    { foundations: 150, height: 150},
+    { foundations: 100, height: 200},
+    { foundations: 100, height: 300},
+    { foundations: 150, height: 300},
+    { foundations: 150, height: 300},
+    { foundations: 170, height: 200},
+    { foundations: 50, height: 100},
+    { foundations: 50, height: 100},
+    { foundations: 50, height: 100},
+    { foundations: 50, height: 100},
+    { foundations: 50, height: 100},
+    { foundations: 50, height: 100},
+    { foundations: 50, height: 100},
+    { foundations: 50, height: 100},
+    { foundations: 80, height: 100},
+    { foundations: 80, height: 100},
+    { foundations: 80, height: 100},
+    { foundations: 80, height: 100},
+    { foundations: 80, height: 100},
+    { foundations: 80, height: 100},
+    { foundations: 100, height: 200},
+    { foundations: 200, height: 250},
+    { foundations: 100, height: 120},
+    { foundations: 100, height: 100},
+    { foundations: 80, height: 250},
+    { foundations: 90, height: 250},
+    { foundations: 200, height: 250},
+    { foundations: 90, height: 250},
+    { foundations: 90, height: 250},
+    { foundations: 90, height: 250},
+    { foundations: 90, height: 250},
+    { foundations: 90, height: 250},
+    { foundations: 90, height: 250},
+    { foundations: 90, height: 250},
+    { foundations: 90, height: 250},
+    { foundations: 90, height: 250},
+    { foundations: 90, height: 250}
+  ];
+  
   var city = cityArchitect.buildCity(data);
   document.body.appendChild(city);
 
